1e1d2cffe555715d83e4c78d43f671cc885767b9
Jack Douglas <jack.douglas10@outlook.com>
2018-12-21 02:46:47 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,22 @@
 cd ~/var/node/com.vivatrucks.discord
 
-echo "--- Pulling from Master ---" 
+echo "----------------------------------------------------"
+echo "                 Gitlab Auto Deployer               "
+echo "               Created By Jack Douglas              "
+echo "----------------------------------------------------"
+echo ""
+echo "---------------- Pulling from Master ---------------" 
 
 git pull origin master
 
-echo "--- Pulled successfully from master ---"
-
-echo "--- Restarting server ---"
+echo "--------- Pulled successfully from master ----------"
+echo ""
+echo "---------------- Restarting server -----------------"
 
 pm2 restart Discord
 
-echo "--- Server restarted Successfully ---"
-
-echo "--- Deployment successful ---"
+echo "---------- Server restarted Successfully -----------"
+echo ""
+echo "----------------------------------------------------"
+echo "               Auto deploy complete                 "
+echo "----------------------------------------------------"
